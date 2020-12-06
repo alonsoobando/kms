ac60e2f445c939542966c21ea8f6a71c3993d828
alonsoobando <agobando94@gmail.com>
2020-12-06 16:00:14 -0600

    'sdfs'

--- kms/src/NavigationBar.js
+++ kms/src/NavigationBar.js
@@ -1,35 +1,23 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import { House, HouseDoor, HouseDoorFill, HouseFill } from 'react-bootstrap-icons';
+import { House, Person, PersonPlus } from 'react-bootstrap-icons';
 
 function NavigationBar() {
     return (
         <div className="NavigationBar">
             <header className="NavigationBar-header">
                 <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home"><HouseFill /> Home</Navbar.Brand>
+                    <Navbar.Brand href="#home"><House /> Home</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
                         </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                        <Button variant="outline-primary" className="mr-sm-2">Log In <Person /></Button>
+                        <Button variant="outline-primary">Sign Up <PersonPlus /></Button>
                     </Navbar.Collapse>
                 </Navbar>
             </header>
