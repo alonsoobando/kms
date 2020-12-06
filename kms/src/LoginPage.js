dfb8af55de4e2597c26e7d6f75c30d614e50bd09
alonsoobando <agobando94@gmail.com>
2020-12-06 17:29:12 -0600

    'fsdgd'

---
+++ kms/src/LoginPage.js
@@ -0,0 +1,16 @@
+import Form from 'react-bootstrap/Form';
+
+
+
+
+function NavigationBar() {
+    return (
+        <div className="NavigationBar">
+            <header className="NavigationBar-header">
+                <h2></h2>
+            </header>
+        </div>
+    );
+}
+
+export default NavigationBar;
