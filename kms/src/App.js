0f36639052f9297e16f41405398ff6f6f45cac60
alonsoobando <agobando94@gmail.com>
2020-12-06 15:30:56 -0600

    'test'

---
+++ kms/src/App.js
@@ -0,0 +1,15 @@
+import NavigationBar from './NavigationBar.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavigationBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
